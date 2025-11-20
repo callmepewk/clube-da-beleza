58af94e50c5adfa610d478e00b443b2dd3b54195
Pedro Henrique Brezolin de Freitas <pedroowkaotaku@gmail.com>
2025-11-20 01:40:38 +0000

    File changes

--- src/pages/Nurse.jsx
+++ src/pages/Nurse.jsx
@@ -12,7 +12,8 @@ import {
   CheckCircle2,
   PlayCircle,
   FileText,
-  Loader2
+  Loader2,
+  Activity
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
